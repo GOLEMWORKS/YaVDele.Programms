149ad51adaa035896002818824650a648304a43b
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-09-22 22:06:04 +0200

    some annotations

--- YaVDele.CalculatorGrant/Models/GrantCalculatorModel.cs
+++ YaVDele.CalculatorGrant/Models/GrantCalculatorModel.cs
@@ -1,8 +1,12 @@
-﻿namespace YaVDele.CalculatorGrant.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YaVDele.CalculatorGrant.Models
 {
     public class GrantCalculatorModel
     {
+        [Range(0, int.MaxValue, ErrorMessage = "Объём гранта не может быть отрицательным!")]
         double grantAmount {  get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Ваш вклад не может быть отрицательным!")]
         double teamHelp {  get; set; }
     }
 }
