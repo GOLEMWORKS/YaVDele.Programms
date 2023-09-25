6c4dfc4e99feb1f79e2f435d6aada5c9e3cf753e
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-09-26 00:10:10 +0200

    clear old useless stuff

--- YaVDele.CalculatorGrant/Data/FileUpload.cs
+++ YaVDele.CalculatorGrant/Data/FileUpload.cs
@@ -10,58 +10,6 @@ namespace YaVDele.CalculatorGrant.Data
 
     public class FileUpload
     {
-        private long maxFileSize = 1024 * 1024 * 1;
-        private int maxAllowedFiles = 3; 
-        string rootpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Uploads"); //берёт неверную директорию
-        string currentTimestamp = DateTime.Now.ToString("h-mm-s");
-        public async Task LoadFiles(InputFileChangeEventArgs e)
-        {
-                foreach (var file in e.GetMultipleFiles(maxAllowedFiles))
-                {
-                    try
-                    {
-                        string newFileName = Path.ChangeExtension(
-                            Path.GetFileNameWithoutExtension(file.Name) + currentTimestamp,
-                            Path.GetExtension(file.Name));
 
-                        switch (Path.GetExtension(file.Name))
-                        {
-                            case ("json"):
-                                string Jpath = JSONuploaded(newFileName);
-                                FileStream fsJ = new(Jpath, FileMode.Create);
-                                await file.OpenReadStream(maxFileSize).CopyToAsync(fsJ);
-                            break;
-
-                            default:
-                                string Opath = OtherUploaded(newFileName);
-                                FileStream fsO = new(Opath, FileMode.Create);
-                                await file.OpenReadStream(maxFileSize).CopyToAsync(fsO);
-                            break;
-                        }
-                    }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Жопа");
-                        }
-                }
-        }
-
-        private string JSONuploaded(string fileName)
-        {
-            string path = Path.Combine(
-                    rootpath,
-                    "\\JSONs",
-                    fileName);
-            return path;
-        }
-
-        private string OtherUploaded(string fileName)
-        {
-            string path = Path.Combine(
-                    rootpath,
-                    "Other",
-                    fileName);
-            return path;
-        }
     }
 }
