496eb4671a44df2bfd6a632598b51ff873dbb0cf
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-10-06 15:43:40 +0200

    load files on android and win 7+

--- YaVDele.CalculatorGrant/Data/FileUpload.cs
+++ YaVDele.CalculatorGrant/Data/FileUpload.cs
@@ -10,10 +10,20 @@ namespace YaVDele.CalculatorGrant.Data
 
     public class FileUpload
     {
-        public async Task LoadFiles(InputFileChangeEventArgs eventArgs) {  
-            string mainDir = FileSystem.Current.AppDataDirectory;
-            string fileName = eventArgs.File.Name;
-            string currentFilePath = Path.Combine(mainDir, fileName);
+        public async Task LoadFiles(IReadOnlyList<IBrowserFile> files) {
+
+            foreach (var file in files)
+            {
+                string mainDir = FileSystem.Current.AppDataDirectory;
+                string fileName = file.Name;
+                string currentFilePath = Path.Combine(mainDir, fileName);
+
+                Stream stream = file.OpenReadStream();
+                FileStream fs = File.Create(currentFilePath);
+                await stream.CopyToAsync(fs);
+                stream.Close();
+                fs.Close();
+            }
         }
 
         public string mainDir()
