24ef2d00e8433629424b13d353b4059d57850b9e
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-10-21 20:14:52 +0200

    список файлов в папке

--- YaVDele.CalculatorGrant/Data/FileUpload.cs
+++ YaVDele.CalculatorGrant/Data/FileUpload.cs
@@ -2,11 +2,6 @@
 
 namespace YaVDele.CalculatorGrant.Data
 {
-    //Загрузить файл в папку
-    //Прочитать его содержимое
-    //Десериализовать JSON
-    //Работа с массивом, его вывод и т.д.
-
     public class FileUpload
     {
         public async Task LoadFiles(IReadOnlyList<IBrowserFile> files) {
--- YaVDele.CalculatorGrant/Data/FolderVisualisation.cs
+++ YaVDele.CalculatorGrant/Data/FolderVisualisation.cs
@@ -2,19 +2,12 @@
 {
     public class FolderVisualisation
     {
-        public string FolderPath { get; set; }
-        public string FolderName { get; set; } 
-        FolderVisualisation(string folderPath, string folderName)
-        {
-            FolderPath = folderPath;
-            FolderName = folderName;
-        }
-
         public string[] FileNamesInFolder(string folderPath)
         {
             string[] files = new string[Directory.GetFiles(folderPath).Length];
             if(Directory.Exists(folderPath)) files = Directory.GetFiles(folderPath);
             return files;
         }
+
     }
 }
