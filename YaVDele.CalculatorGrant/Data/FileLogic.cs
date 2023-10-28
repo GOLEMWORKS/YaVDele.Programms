2847df954f88f6196640444acfb3a72d15d07993
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-10-28 21:58:48 +0200

    Create delete logic for files

---
+++ YaVDele.CalculatorGrant/Data/FileLogic.cs
@@ -0,0 +1,16 @@
+﻿namespace YaVDele.CalculatorGrant.Data
+{
+    public class FileLogic : FileUpload
+    {
+        public void FileDelete(string fileName)
+        {
+            string pathToDelete = Path.Combine(MainDirInit(), fileName);
+            Directory.Delete(pathToDelete);
+        }
+
+        public void FileReadAsTable(string path, string fileName)
+        {
+
+        }
+    }
+}
