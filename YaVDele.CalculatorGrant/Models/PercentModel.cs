0134f83170a90a8ea0f322490a59ab6f5a021d1a
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-11-02 12:49:53 +0200

    Percentage form create

---
+++ YaVDele.CalculatorGrant/Models/PercentModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YaVDele.CalculatorGrant.Models
+{
+    public class PercentModel
+    {
+        public double firstAmount { get; set; }
+        [Range(0, 100, ErrorMessage = "Это проценты. Максимум 100, минимум 0.")]
+        public int percent { get; set; }
+    }
+}
