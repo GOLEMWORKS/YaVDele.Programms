27dd03f8900c7bb4b1ad2df1aabe91eb4e7d746a
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-11-06 19:27:59 +0200

    Indexation_fin/ Create alpha build

--- YaVDele.CalculatorGrant/Models/IndexationModel.cs
+++ YaVDele.CalculatorGrant/Models/IndexationModel.cs
@@ -5,11 +5,11 @@ namespace YaVDele.CalculatorGrant.Models
 {
     public class IndexationModel
     {
-        [Range(0, int.MaxValue)]
+        [Range(0, int.MaxValue, ErrorMessage = "Цена не может быть отрицательной")]
         public double priceBefore { get; set; }
         [Range(0, int.MaxValue)]
         public double priceAfter { get; set; }
-        [Range(1, int.MaxValue)]
-        public int quantity { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Минимальное значение = 1")]
+        public int quantity { get; set; } = 1;
     }
 }
