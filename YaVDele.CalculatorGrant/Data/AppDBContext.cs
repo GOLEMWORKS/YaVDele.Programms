3eccf31fc0dedc7d22f705d7be6a9c9b46712d26
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-11-25 14:56:44 +0200

    Methods to CRUD in database

--- YaVDele.CalculatorGrant/Data/AppDBContext.cs
+++ YaVDele.CalculatorGrant/Data/AppDBContext.cs
@@ -23,5 +23,23 @@ namespace YaVDele.CalculatorGrant.Data
             await Init();
             return await Database.Table<VolounteresJobList>().ToListAsync();
         }
+        public async Task<VolounteresJobList> GetItemsAsync(int id)
+        {
+            await Init();
+            return await Database.Table<VolounteresJobList>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task<int> SaveItemsAsync(VolounteresJobList item)
+        {
+            await Init();
+            if(item.Id != 0)
+                return await Database.UpdateAsync(item);
+            else
+                return await Database.InsertAsync(item);
+        }
+        public async Task<int> DeleteItemAsync(VolounteresJobList item)
+        {
+            await Init();
+            return await Database.DeleteAsync(item);
+        }
     }
 }
